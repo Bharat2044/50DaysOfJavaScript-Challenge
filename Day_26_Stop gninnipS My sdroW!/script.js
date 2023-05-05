83393b75a9505fa0f5a88ad9e6361861e2993272
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-05-05 21:00:38 +0530

    code added

---
+++ Day_26_Stop gninnipS My sdroW!/script.js
@@ -0,0 +1,22 @@
+// spinWords("Hey fellow warriors") should return"Hey wollef sroirraw"
+// spinWords("You are almost to the last test") should return "You are tsomla to the last test"
+// spinWords("Seriously this is the last one") should return "ylsuoireS this is the last one"
+
+
+function spinWords(str){
+    let string = str;
+    let final = [];
+    let strList = str.split(' ');
+    
+    for(var i = 0; i<strList.length; i++) {
+        
+        if(strList[i].length>4 )
+            final.push(strList[i].split('').reverse().join(''));
+        else
+            final.push(strList[i]);
+    }
+    
+    return final.join(' ');
+ }
+
+console.log(spinWords("Seriously this is the last one"));
